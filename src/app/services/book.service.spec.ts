7f07855cea9d8f36211e4938b86088cba077ff5b
JCamiloo <jcamilo.osorio15@gmail.com>
2022-01-13 21:33:44 -0500

    testing service

--- src/app/services/book.service.spec.ts
+++ src/app/services/book.service.spec.ts
@@ -4,6 +4,7 @@ import { TestBed } from "@angular/core/testing";
 import { BookService } from './book.service';
 import { Book } from '../models/book.model';
 import { environment } from '../../environments/environment';
+import swal from 'sweetalert2';
 
 const BOOKS: Book[] = [
   {
@@ -38,6 +39,11 @@ describe('BookService', () => {
   });
 
   afterEach(() => {
+    jest.resetAllMocks();
+    localStorage.clear();
+  });
+
+  afterAll(() => {
     httpMock.verify();
   });
 
@@ -53,5 +59,72 @@ describe('BookService', () => {
     const testRequest = httpMock.expectOne(environment.API_REST_URL + '/book');
     expect(testRequest.request.method).toBe('GET');
     testRequest.flush(BOOKS);
+  });
+
+  it('getBooksFromCart return a empty array when localstorage is empty', () => {
+    const listBook = service.getBooksFromCart();
+
+    expect(listBook.length).toBe(0);
+  });
+
+  it('getBooksFromCart return an array when it exists in the localstorage', () => {
+    localStorage.setItem('listCartBook', JSON.stringify(BOOKS));
+
+    const listBook = service.getBooksFromCart();
+
+    expect(listBook.length).toBe(2);
+  });
+
+  it('addBookToCart add a book successfully when the list does not exist in the localStorage', () => {
+    const book: Book =   {
+      name: '',
+      author: '',
+      isbn: '',
+      price: 15000,
+    };
+
+    const toastMock = { fire: () => null } as any;
+
+    const toastSpy = jest.spyOn(swal, 'mixin').mockImplementation(() => {
+      return toastMock;
+    });
+
+    let newListBook = service.getBooksFromCart();
+
+    expect(newListBook.length).toBe(0);
+
+    service.addBookToCart(book);
+    newListBook = service.getBooksFromCart();
+
+    expect(toastSpy).toBeCalledTimes(1);
+    expect(newListBook.length).toBe(1);
+  });
+
+  it('removeBooksFromCart removes the list from the localStorage', () => {
+    const book: Book = {
+      name: '',
+      author: '',
+      isbn: '',
+      price: 15000,
+      amount: 2
+    };
+
+    const toastMock = { fire: () => null } as any;
+
+    jest.spyOn(swal, 'mixin').mockImplementation(() => {
+      return toastMock;
+    });
+
+
+    service.addBookToCart(book);
+    let newListBook = service.getBooksFromCart();
+
+    expect(newListBook.length).toBe(1);
+
+    service.removeBooksFromCart();
+
+    newListBook = service.getBooksFromCart();
+
+    expect(newListBook.length).toBe(0);
   });
 });
